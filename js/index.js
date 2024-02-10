308bda1ff3b5ff7670d30259ae6dfebc00f85f14
BashBuilder <iamadelowotan@gmail.com>
2024-02-10 10:08:21 +0100

    navbar update

--- js/index.js
+++ js/index.js
@@ -1,6 +1,9 @@
 const sidebar = document.getElementById("sidebar");
 const openSidebar = document.getElementById("open-sidebar");
 const closeSidebar = document.getElementById("close-sidebar");
+const sidebar2 = document.getElementById("sidebar2");
+const openSidebar2 = document.getElementById("open-sidebar-2");
+const closeSidebar2 = document.getElementById("close-sidebar-2");
 
 openSidebar.addEventListener("click", () => {
   sidebar.classList.add("sidebar-open");
@@ -11,4 +14,13 @@ closeSidebar.addEventListener("click", () => {
   sidebar.classList.remove("sidebar-open");
   openSidebar.classList.add("animation-rotate");
   closeSidebar.classList.remove("animation-rotate");
+});
+openSidebar2.addEventListener("click", () => {
+  sidebar2.classList.add("sidebar2-open");
+  closeSidebar2.classList.add("animation-rotate");
+});
+
+closeSidebar2.addEventListener("click", () => {
+  sidebar2.classList.remove("sidebar2-open");
+  closeSidebar2.classList.remove("animation-rotate");
 });
