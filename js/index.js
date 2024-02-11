9af577de5760473732451ad34f488c25891fb73a
BashBuilder <iamadelowotan@gmail.com>
2024-02-11 09:41:14 +0100

    close sidebar update

--- js/index.js
+++ js/index.js
@@ -1,28 +1,48 @@
 const sidebar = document.getElementById("sidebar");
-const openSidebar = document.getElementById("open-sidebar");
-const closeSidebar = document.getElementById("close-sidebar");
+const openSidebarBtn = document.getElementById("open-sidebar");
+const closeSidebarBtn = document.getElementById("close-sidebar");
 const sidebar2 = document.getElementById("sidebar2");
-const openSidebar2 = document.getElementById("open-sidebar-2");
-const closeSidebar2 = document.getElementById("close-sidebar-2");
+const openSidebar2Btn = document.getElementById("open-sidebar-2");
+const closeSidebar2Btn = document.getElementById("close-sidebar-2");
 
-openSidebar.addEventListener("click", () => {
+const openSidebar = () => {
   sidebar.classList.add("sidebar-open");
-  openSidebar.classList.remove("animation-rotate");
-  closeSidebar.classList.add("animation-rotate");
+  openSidebarBtn.classList.remove("animation-rotate");
+  closeSidebarBtn.classList.add("animation-rotate");
   document.body.style.overflow = "hidden";
-});
-closeSidebar.addEventListener("click", () => {
+};
+const closeSidebar = () => {
   sidebar.classList.remove("sidebar-open");
-  openSidebar.classList.add("animation-rotate");
-  closeSidebar.classList.remove("animation-rotate");
-});
-openSidebar2.addEventListener("click", () => {
-  sidebar2.classList.add("sidebar2-open");
-  closeSidebar2.classList.add("animation-rotate");
-  document.body.style.overflow = "hidden";
-});
+  openSidebarBtn.classList.add("animation-rotate");
+  closeSidebarBtn.classList.remove("animation-rotate");
+  document.body.style.overflow = "auto";
+};
 
-closeSidebar2.addEventListener("click", () => {
+const openSidebar2 = () => {
+  sidebar2.classList.add("sidebar2-open");
+  closeSidebar2Btn.classList.add("animation-rotate");
+  document.body.style.overflow = "hidden";
+};
+const closeSidebar2 = () => {
   sidebar2.classList.remove("sidebar2-open");
-  closeSidebar2.classList.remove("animation-rotate");
+  closeSidebar2Btn.classList.remove("animation-rotate");
+  document.body.style.overflow = "auto";
+};
+
+openSidebarBtn.addEventListener("click", () => openSidebar());
+closeSidebarBtn.addEventListener("click", () => closeSidebar());
+openSidebar2Btn.addEventListener("click", () => openSidebar2());
+closeSidebar2Btn.addEventListener("click", () => closeSidebar2());
+
+// Event listener to close the sidebar when clicking outside
+document.addEventListener("click", function (event) {
+  const sidebarPosition = sidebar.getBoundingClientRect().x;
+  const sidebar2Position = sidebar2.getBoundingClientRect().right;
+  console.log(sidebar2Position);
+  if (!sidebar.contains(event.target) && sidebarPosition === 0) {
+    closeSidebar();
+  }
+  if (!sidebar2.contains(event.target) && sidebar2Position > 50) {
+    closeSidebar2();
+  }
 });
